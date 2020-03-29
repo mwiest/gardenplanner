85389882e9ad6d2dc6a3d0489b3b028313d9ef3a
Marc Wiest <m.wiest@gmx.ch>
2020-03-29 21:06:55 +0200

    Refresh cache better, trial and error.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -18,6 +18,8 @@ if (process.env.NODE_ENV === "production") {
     },
     updatefound() {
       console.log("New content is downloading.");
+      // new update available
+      setTimeout(() => document.location.reload(true), 1000);
     },
     updated() {
       console.log("New content is available; please refresh.");
